dec7edd22e745355a1a93ffc9b83806b29997da7
samu1243 <samueldjekki@gmail.com>
2022-04-12 20:39:17 -0400

    creacion de carta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,51 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function numeroEntero(min, max) {
+  return ~~(Math.random() * (max - min + 1)) + min;
+}
+function signosRandom() {
+  let signos = ["♠", "♥", "♦", "♣"];
+  let random = numeroEntero(0, 3);
+  return signos[random];
+}
+
+function numeroRandom() {
+  let numeroRandom = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numeritosCentral = numeroEntero(0, 12);
+  return numeroRandom[numeritosCentral];
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let signo = signosRandom();
+  let numero = numeroRandom();
+  let numeritosCentral = document.getElementById("numeritosCentral");
+  numeritosCentral.innerHTML = numero;
+  let signoSuperior = document.getElementById("signoSuperior");
+  let signoInferior = document.getElementById("signoInferior");
+  signoSuperior.innerHTML = signo;
+  signoInferior.innerHTML = signo;
+  document.getElementById("signoInferior").innerHTML = signo;
+  if (signo === "♥") {
+    signoSuperior.style.color = "red";
+    signoInferior.style.color = "red";
+  }
+  if (signo === "♦") {
+    signoSuperior.style.color = "red";
+    signoInferior.style.color = "red";
+  }
 };
